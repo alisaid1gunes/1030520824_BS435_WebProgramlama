2ed565aa9394dd64452d78b4bd72124f978df3ec
said <alisaidgunes1@gmail.com>
2022-11-15 00:34:06 +0300

    add: Views.java

---
+++ src/main/java/org/webp/Views.java
@@ -0,0 +1,15 @@
+package org.webp;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+public class Views {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "viewDate", nullable = false)
+    private Date viewDate;
+}
