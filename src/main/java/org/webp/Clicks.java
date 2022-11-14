2897afe0f333eae92c03d877bdd85a73bef4d3c2
said <alisaidgunes1@gmail.com>
2022-11-15 00:35:04 +0300

    add: CLicks.java

---
+++ src/main/java/org/webp/Clicks.java
@@ -0,0 +1,15 @@
+package org.webp;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+public class Clicks {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "clickDate", nullable = false)
+    private Date clickDate;
+}
